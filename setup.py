0791f37aae32d98f62b76d1e60d08397f70d2378
= <=>
2025-08-12 19:07:25 +0530

    Packaging config for Cython build

--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
-from setuptools import setup
+# from setuptools import setup
+# from Cython.Build import cythonize
+
+# setup(
+#     ext_modules=cythonize(
+#         "src/payments/*.pyx",  # path to your Cython files
+#         compiler_directives={"language_level": "3"},
+#     ),
+#     packages=["payments"],
+#     package_dir={"": "src"},
+# )
+
+
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
+import glob
+
+ext_modules = cythonize(
+    glob.glob("src/payments/*.pyx"),
+    compiler_directives={"language_level": "3"}
+)
 
 setup(
-    ext_modules=cythonize(
-        "src/payments/*.pyx",  # path to your Cython files
-        compiler_directives={"language_level": "3"},
-    ),
-    packages=["payments"],
+    name="payments",
+    version="0.1.0",
     package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    ext_modules=ext_modules,
+    include_package_data=True,
 )
