ba132131bd11d81913b919ecc4556275e48213f7
= <=>
2025-08-13 10:10:55 +0530

    Testing github workflow build v2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ ext_modules = cythonize(
 
 setup(
     name="payments",
-    version="0.2.4",
+    version="0.2.5",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     ext_modules=ext_modules,
