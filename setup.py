6234ed4726a99ee56b31721ad848af8c274284b8
= <=>
2025-09-30 16:55:33 +0530

    Added sentence_transformers in requirements

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ ext_modules = cythonize(
 
 setup(
     name="payments",
-    version="2.2.1",
+    version="2.3.2",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     ext_modules=ext_modules,
